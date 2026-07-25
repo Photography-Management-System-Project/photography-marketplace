6366eb39af441bb19c816b3aa74a316c6b9fbdac
Omkar_Gaikwad <og950597@gmail.com>
2026-07-25 10:11:50 +0530

    Delete frontend directory

--- frontend/src/Components/notifications/ToastHost.jsx
+++
@@ -1,5 +0,0 @@
-// Toast rendering lives inside ToastContext for simplicity;
-// this file is kept as an extension point for future notification UIs.
-export default function ToastHost() {
-  return null
-}
